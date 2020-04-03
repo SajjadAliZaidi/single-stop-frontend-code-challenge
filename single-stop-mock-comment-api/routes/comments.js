39bd4f66f6dc70972287b698e4a758cdf2f290e0
Neil Girardi <neilgirardi@Neils-MacBook-Pro.local>
2020-04-03 15:46:58 -0400

    Add validation to mock API.

--- single-stop-mock-comment-api/routes/comments.js
+++ single-stop-mock-comment-api/routes/comments.js
@@ -2,12 +2,28 @@ var express = require('express')
 var router = express.Router()
 const commentsController = require('./../controllers/comments')
 
-/* GET users listing. */
 router.get('/', (req, res, next) => res.json(commentsController.getComments()))
 
 router.post('/', (req, res, next) => {
-  const comment = commentsController.addComment(req.body.comment)
-  res.json(comment)
+  const { comment: requestComment } = req.body
+
+  if (!requestComment) {
+    return res.status(400).send({error: 'Malformed request payload. The correct shape is: { "comment": { "body": "hello world", "email": "johndoe@gmail.com"}'})
+  }
+
+  const { body, email } = requestComment
+
+  if (!body) {
+    return res.status(400).send({error: 'comment.body is a required field'})
+  }
+
+  if (!email) {
+    return res.status(400).send({error: 'comment.email is a required field'})
+  }
+
+  const responseComment = commentsController.addComment(requestComment)
+
+  return res.status(201).json(responseComment)
 })
 
 router.delete('/:id', (req, res, next) => {
