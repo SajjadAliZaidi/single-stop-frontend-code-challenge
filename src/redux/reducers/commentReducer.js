14bbed678731c5a2d3026917284a30ed70cc48c7
SajjadAliZaidi <sajjadalizaidi00@gmail.com>
2021-08-14 16:33:19 +0500

    comments now being persisted to local storage

--- src/redux/reducers/commentReducer.js
+++ src/redux/reducers/commentReducer.js
@@ -1,22 +1,20 @@
-const INITIAL_STATE = {
-}
+const INITIAL_STATE = JSON.parse(localStorage.getItem('comments-app-comments')) ?
+  JSON.parse(localStorage.getItem('comments-app-comments')) : {};
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "ADD": {
       const id = action.payload.id;
       const newComment = action.payload;
-      return {
-        ...state,
-        [id]: newComment
-      };
+      const newState = { ...state, [id]: newComment };
+      localStorage.setItem('comments-app-comments', JSON.stringify(newState))
+      return newState;
     }
     case "DELETE": {
       const newState = { ...state };
       delete newState[action.payload.id];
-      return {
-        ...newState
-      };
+      localStorage.setItem('comments-app-comments', JSON.stringify(newState))
+      return newState;
     }
     default:
       return state;
